915f9eb83c608558480a6b85fb3c2eb89f3e325d
abdullinn4 <abdullin.nargiz@yandex.ru>
2025-07-11 12:22:06 +0300

    added style for Toolbar.Overview

--- unitap_frontend/src/widgets/cardOverview/CardOverview.tsx
+++ unitap_frontend/src/widgets/cardOverview/CardOverview.tsx
@@ -8,15 +8,27 @@ export const CardOverview = ({onSelectSection}: Props) => {
     const handleSave = () => {}
 
     return (
-        <div className={style.overview_wrapper}>
-            <button onClick={() => onSelectSection("settings")} className={style.overview_button}>Настройки</button>
-            <button onClick={() => onSelectSection("viewer")} className={style.overview_button}>Режим зрителя</button>
-            <button onClick={() => onSelectSection("help")} className={style.overview_button}>Помощь</button>
+        <>
+            <div className={style.overview_wrapper}>
+                <div className={style.section_row}>
+                    <img src="src/assets/settings.svg" alt="settings"/>
+                    <button onClick={() => onSelectSection("settings")} className={style.overview_button}>Настройки</button>
+                </div>
+                <div className={style.section_row}>
+                    <img src="src/assets/viewer.svg" alt="viewer"/>
+                    <button onClick={() => onSelectSection("viewer")} className={style.overview_button}>Режим зрителя</button>
+                </div>
+                <div className={style.section_row}>
+                    <img src="src/assets/help.svg" alt="help"/>
+                    <button onClick={() => onSelectSection("help")} className={style.overview_button}>Помощь</button>
+                </div>
+            </div>
 
             <button onClick={handleSave} className={style.overview_save_button}>
                 <p>Сохранить</p>
                 <p>Введите название</p>
             </button>
-        </div>
+        </>
+
     )
 }
